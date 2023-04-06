312a5e2642ae7ffc5aebcf38fd14523dea753ded
Chris Lupton <42554017+clupt@users.noreply.github.com>
2023-04-06 13:42:06 -0500

    add initial files

---
+++ routes/companies.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const express = require("express");
+const { NotFoundError, BadRequestError } = require("./expressError");
+
+const companiesRouter = new express.Router();
+const db = require("../db");
+
+
+
+
+
+
+
+
+
+
+
+
+
+modules.exports = companiesRouter;
